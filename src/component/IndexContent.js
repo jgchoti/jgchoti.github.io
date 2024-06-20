93c69f9a49678695b27303fe67400a0107e93a00
jgchoti <jgchotirat@gmail.com>
2024-06-20 20:50:58 +0200

    Create IndexContent.js

---
+++ src/component/IndexContent.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import ProjectShowcase from './ProjectShowcase';
+import { projectData } from '../data/projectData.js';
+
+function IndexContent() {
+    return (
+        <div className="container content-container">
+            <div className="row mt-5">
+                {projectData.map((project, index) => (
+                    <ProjectShowcase
+                        key={index}
+                        name={project.name}
+                        imageUrl={project.imageUrl}
+                        linkUrl={project.linkUrl}
+                        linkTitle={project.linkTitle}
+                        shortDescription={project.shortDescription}
+                    />
+                ))}
+
+            </div>
+        </div>
+    );
+}
+
+export default IndexContent;
