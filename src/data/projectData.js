3aa3a0d62d5680d9340a41b786cb3c8f81b053b6
jgchoti <jgchotirat@gmail.com>
2024-06-20 20:46:22 +0200

    Create projectData.js

---
+++ src/data/projectData.js
@@ -0,0 +1,47 @@
+export const projectData = [
+    {
+        name: "Online Resume",
+        description: "In this project for beginners, I took my first steps into the exciting world of web development. I learned the ABCs of HTML, CSS, and JavaScript while making my very own landing page.",
+        details: "So, what's the big deal about this project? Well, it's like the start of my journey in web development. Check out the landing page I created – it's where I laid the foundation for all the coding adventures I've been having since then.",
+        imageUrl: process.env.PUBLIC_URL + "/assets/images/cv_website.png",
+        linkUrl: "https://chotirat.netlify.app/",
+        linkTitle: "Chotirat's Online Resume",
+        shortDescription: "Built with HTML, CSS and JavaScript"
+    },
+    {
+        name: "Weather App",
+        description: "In this project, I jumped into the world of live API to make a dynamic weather app. I aimed for a design that's user-friendly. It shows the weather info in a way that's not only easy on the eyes but also easy to understand. People can get the latest on weather conditions, forecasts, and what's happening in their area, all thanks to the API integration.",
+        details: "While working on this, I got the hang of handling requests to the API and even learned how to tackle any hiccups along the way. This project basically shows off my knack for seamlessly putting together live APIs, so that users can get the freshest and most accurate weather details.",
+        imageUrl: process.env.PUBLIC_URL + "/assets/images/weather.png",
+        linkUrl: "https://weather2023.netlify.app/",
+        linkTitle: "Weather App Project",
+        shortDescription: "Exploring the Power of Live API Integration"
+    },
+    {
+        name: "Weather App 2.0",
+        description: "Utilizing my knowledge of HTML, CSS, and JavaScript, I embarked on building the Weather App using React. This project marked my entry into the world of React.",
+        details: "Through this project, I deepened my understanding of React's core concepts, such as state, props, and components. This project reflects my growth as a developer and my ability to adapt and apply new technologies.",
+        imageUrl: process.env.PUBLIC_URL + "/assets/images/weather2.png",
+        linkUrl: "https://weather2024.netlify.app/",
+        linkTitle: "Weather App Project create with React",
+        shortDescription: " Built with HTML, CSS with React"
+    },
+    {
+        name: "English Dictionary",
+        description: "Dive into the wordy world with this awesome English Dictionary App. So, I wanted to spice up my web development skills, and what better way than crafting this dictionary using React, right? One of the standout features of the app is the random word function. It's like a surprise button that makes learning fun and unexpected!",
+        details: "Beyond just looking up meanings, users can also get random words thrown their way, making their learning journey exciting and engaging. And hey, the dictionary API integration was a fun challenge to crack.",
+        imageUrl: process.env.PUBLIC_URL + "/assets/images/dictionary.png",
+        linkUrl: "https://reactdictionary.netlify.app/",
+        linkTitle: "English Dictionary Project create with React",
+        shortDescription: " Built with HTML, CSS with React"
+    },
+    {
+        name: "Bike Shop Website",
+        description: "I made a website for a local Bike Repair Shop. I used HTML, CSS, JavaScript, and Bootstrap to make this site user-friendly and responsive.",
+        details: "The goal is to make this site looks great on different devices, and to help the shop connect with customers in a digital world.This project.",
+        imageUrl: process.env.PUBLIC_URL + "/assets/images/fietsateljee_berchem.png",
+        linkUrl: "https://fietsateljee-berchem.netlify.app/",
+        linkTitle: "Bike Shop Website",
+        shortDescription: " A responsive website, built with HTML, CSS, and JavaScript.t"
+    }
+];
