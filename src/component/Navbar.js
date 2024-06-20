ed9ffc31fa30e94450181c2ee8928b4bb6b49cc0
jgchoti <jgchotirat@gmail.com>
2024-06-20 20:46:35 +0200

    Create Navbar.js

---
+++ src/component/Navbar.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+
+function Navbar() {
+    return (
+        <nav className="navbar navbar-expand-sm bg-body-tertiary fixed-top">
+            <div className="container-fluid px-5">
+                <NavLink className="navbar-brand" to="/" title="Homepage">
+                    <img src={process.env.PUBLIC_URL + '/assets/images/logo_choti.png'} alt="Choti Logo" className="logo" />
+                </NavLink>
+                <button
+                    className="navbar-toggler"
+                    type="button"
+                    data-bs-toggle="collapse"
+                    data-bs-target="#navbarNavAltMarkup"
+                    aria-controls="navbarNavAltMarkup"
+                    aria-expanded="false"
+                    aria-label="Toggle navigation"
+                >
+                    <span className="navbar-toggler-icon"></span>
+                </button>
+                <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+                    <div className="navbar-nav">
+                        <NavLink exact className="nav-link" to="/" title="Homepage">Home</NavLink>
+                        <NavLink className="nav-link" to="/project" title="Choti's Projects">Work</NavLink>
+                        <NavLink className="nav-link" to="/about" title="About Choti">About</NavLink>
+                        <NavLink className="nav-link" to="/contact" title="Contact Choti">Contact</NavLink>
+                    </div>
+                </div>
+            </div>
+        </nav>
+    );
+}
+
+export default Navbar;
