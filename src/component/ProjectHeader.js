f4b55689cd9e1170ffa0cb1eb1a3e9924befe2e5
jgchoti <jgchotirat@gmail.com>
2024-06-20 20:46:27 +0200

    Create ProjectHeader.js

---
+++ src/component/ProjectHeader.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+
+function ProjectHeader() {
+    return (
+        <div className="hero">
+            <h1>Explore My Project</h1>
+        </div>
+    );
+}
+
+export default ProjectHeader;
