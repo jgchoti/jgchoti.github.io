a619fdff9717f0597471ec5bcd2f6391eab7f598
jgchoti <jgchotirat@gmail.com>
2024-06-21 15:46:12 +0200

    Update ProjectsContent.js

--- src/component/ProjectsContent.js
+++ src/component/ProjectsContent.js
@@ -1,6 +1,9 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faGithub } from '@fortawesome/free-brands-svg-icons'
+import { faDisplay } from '@fortawesome/free-solid-svg-icons'
 
-function ProjectContent({ name, description, details, imageUrl, linkUrl, linkTitle, index }) {
+function ProjectContent({ name, description, details, imageUrl, linkUrl, linkTitle, index, githubUrl }) {
     const id = name.split(' ').join('').toLowerCase();
     const isEvenProject = index % 2 === 0;
 
@@ -27,15 +30,12 @@ function ProjectContent({ name, description, details, imageUrl, linkUrl, linkTit
                     <div className="collapse multi-collapse" id={id}>
                         <p>{details}</p>
                     </div>
-                    <div className="m-5">
-                        <a
-                            href={linkUrl}
-                            className="btn-branding-outline"
-                            target="_blank"
-                            rel="noreferrer"
-                            title={linkTitle}
-                        >
-                            Launch {name}
+                    <div className="mt-4 d-flex justify-content-center">
+                        <a href={githubUrl} target="_blank" rel="noopener noreferrer" className="btn-branding-outline" title="GitHub">
+                            <FontAwesomeIcon icon={faGithub} /> <p className='btn-text mb-0 ms-3'>GitHub</p>
+                        </a>
+                        <a href={linkUrl} target="_blank" rel="noopener noreferrer" className="btn-branding-outline" title="Demo">
+                            <FontAwesomeIcon icon={faDisplay} /> <p className='btn-text mb-0 ms-3'>Launch {name}</p>
                         </a>
                     </div>
                 </div>
