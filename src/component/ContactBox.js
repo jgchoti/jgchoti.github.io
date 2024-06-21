48d82a24000b0c6018c77d02bb6afdebb7700b51
jgchoti <jgchotirat@gmail.com>
2024-06-21 15:46:06 +0200

    Update ContactBox.js

--- src/component/ContactBox.js
+++ src/component/ContactBox.js
@@ -3,15 +3,15 @@ import { Link } from 'react-router-dom';
 
 function ContactBox() {
     return (
-        <div className="contact-box d-flex justify-content-between">
-            <div>
-                <h4>Work Inquiry</h4>
-                <p className="text-muted">Interested in working together?</p>
+        <div className="contact-box d-flex flex-column flex-sm-row justify-content-between align-items-center">
+            <div className='d-flex flex-column flex-sm-row align-items-center mb-3 mb-sm-0'>
+                <h4 className="mb-1 mb-sm-0 me-sm-3">Work Inquiry</h4>
+                <p className="text-muted mb-0">Interested in working together?</p>
             </div>
-            <div>
+            <div className='d-flex flex-column flex-sm-row align-items-center'>
                 <Link
                     to="/contact"
-                    className="btn btn-branding"
+                    className="btn-branding"
                     title="Contact Choti"
                 >
                     Contact Me
@@ -22,4 +22,3 @@ function ContactBox() {
 }
 
 export default ContactBox;
-
