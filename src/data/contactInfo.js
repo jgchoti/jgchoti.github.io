1f0e88976dee387b6562c10dba711b62da36ef3d
jgchoti <jgchotirat@gmail.com>
2024-06-21 11:20:19 +0200

    Create contactInfo.js

---
+++ src/data/contactInfo.js
@@ -0,0 +1,33 @@
+import { faSquareGithub, faLinkedin, faSquareInstagram } from '@fortawesome/free-brands-svg-icons'
+import { faEnvelope } from '@fortawesome/free-solid-svg-icons'
+
+export const contactInfo = [
+    {
+        platform: 'GitHub',
+        name: 'github.com/jgchoti',
+        link: "https://github.com/jgchoti",
+        title: "Visit Choti's GitHub profile",
+        icon: faSquareGithub
+    },
+    {
+        platform: 'Linkedin',
+        name: 'linkedin.com/in/chotirat',
+        link: "https://www.linkedin.com/in/chotirat/",
+        title: "Visit Choti's LinkedIn profile",
+        icon: faLinkedin
+    }, {
+        platform: 'Instagram',
+        name: '@jgchoti',
+        link: "https://www.instagram.com/jgchoti/",
+        title: "Visit Choti's Instagram profile",
+        icon: faSquareInstagram
+    }
+    , {
+        platform: 'E-mail',
+        name: 'jgchotirat@gmail.com',
+        link: "mailto:jgchotirat@gmail.com",
+        title: "Choti's E-mail",
+        icon: faEnvelope
+    }
+]
+
