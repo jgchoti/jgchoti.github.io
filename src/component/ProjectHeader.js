2522469c01a95d48584e3f23ce9a85e2f81dd9b2
jgchoti <jgchotirat@gmail.com>
2024-06-21 11:34:50 +0200

    Update ProjectHeader.js

--- src/component/ProjectHeader.js
+++ src/component/ProjectHeader.js
@@ -1,10 +1,15 @@
 import React from 'react';
-
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faAnglesDown } from '@fortawesome/free-solid-svg-icons'
 
 function ProjectHeader() {
     return (
         <div className="hero">
             <h1>Explore My Project</h1>
+            <div className='scroll-text-wrapper'>
+                <FontAwesomeIcon icon={faAnglesDown} />
+                <h4>scroll down to see more</h4>
+            </div>
         </div>
     );
 }
