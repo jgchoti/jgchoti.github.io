49bf07b81ffb6ad3d0826faa67a02e89e21ed06c
jgchoti <jgchotirat@gmail.com>
2024-06-23 11:53:19 +0200

    Update projectData.js

--- src/data/projectData.js
+++ src/data/projectData.js
@@ -1,8 +1,8 @@
 export const projectData = [
     {
         name: "Online Resume",
-        description: "In this project for beginners, I took my first steps into the exciting world of web development. I learned the ABCs of HTML, CSS, and JavaScript while making my very own landing page.",
-        details: "So, what's the big deal about this project? Well, it's like the start of my journey in web development. Check out the landing page I created – it's where I laid the foundation for all the coding adventures I've been having since then.",
+        description: "My first ever landing page! This project marks my initial steps into the exciting world of web development, primarily using vanilla HTML and CSS with a bit of JavaScript. I learned the basics of HTML, CSS, and JavaScript while creating my very own landing page.",
+        details: "So, what's the big deal about this project? Well, it's like the start of my journey in web development. It may not be the most thrilling, but you can still check out my CV and see the landing page I created! It's where I laid the foundation for all the coding adventures I've been having since then.",
         imageUrl: process.env.PUBLIC_URL + "/assets/images/cv_website.png",
         linkUrl: "https://chotirat.netlify.app/",
         githubUrl: 'https://github.com/jgchoti/cv',
@@ -11,8 +11,8 @@ export const projectData = [
     },
     {
         name: "Weather App",
-        description: "In this project, I jumped into the world of live API to make a dynamic weather app. I aimed for a design that's user-friendly. It shows the weather info in a way that's not only easy on the eyes but also easy to understand. People can get the latest on weather conditions, forecasts, and what's happening in their area, all thanks to the API integration.",
-        details: "While working on this, I got the hang of handling requests to the API and even learned how to tackle any hiccups along the way. This project basically shows off my knack for seamlessly putting together live APIs, so that users can get the freshest and most accurate weather details.",
+        description: "In this project, I got into the world of live API to make a dynamic weather app. I aimed for a design that's user-friendly. User can get the latest on weather conditions, forecasts, and what's happening in their area, all thanks to the API integration.",
+        details: "My primary focus was on integrating weather APIs to ensure users receive up-to-date and accurate weather information. This involved learning how to effectively manage data from external sources and present it in a user-friendly format within the application. By mastering API interactions, I not only enhanced my technical skills in JavaScript but also learned the importance of data integrity and real-time updates in user-facing applications. Moving forward, I plan to further refine this project by exploring additional APIs and improving the overall user experience.",
         imageUrl: process.env.PUBLIC_URL + "/assets/images/weather.png",
         linkUrl: "https://weather2023.netlify.app/",
         githubUrl: 'https://github.com/jgchoti/weather-app',
@@ -22,7 +22,7 @@ export const projectData = [
     {
         name: "Weather App 2.0",
         description: "Utilizing my knowledge of HTML, CSS, and JavaScript, I embarked on building the Weather App using React. This project marked my entry into the world of React.",
-        details: "Through this project, I deepened my understanding of React's core concepts, such as state, props, and components. This project reflects my growth as a developer and my ability to adapt and apply new technologies.",
+        details: "Through this project, I deepened my understanding of React's core concepts, such as state, props, and components. This project reflects my growth and remind me that software development is a continuous learning process.",
         imageUrl: process.env.PUBLIC_URL + "/assets/images/weather2.png",
         alternate_image: process.env.PUBLIC_URL + '/assets/images/weather2_light.png',
         linkUrl: "https://weather2024.netlify.app/",
@@ -31,15 +31,15 @@ export const projectData = [
         shortDescription: " Built with HTML, CSS with React"
     },
     {
-        name: "English Dictionary",
-        description: "Dive into the wordy world with this awesome English Dictionary App. So, I wanted to spice up my web development skills, and what better way than crafting this dictionary using React, right? One of the standout features of the app is the random word function. It's like a surprise button that makes learning fun and unexpected!",
-        details: "Beyond just looking up meanings, users can also get random words thrown their way, making their learning journey exciting and engaging. And hey, the dictionary API integration was a fun challenge to crack.",
-        imageUrl: process.env.PUBLIC_URL + "/assets/images/dictionary.png",
-        alternate_image: process.env.PUBLIC_URL + "/assets/images/dictionary_dark.png",
-        linkUrl: "https://retro-english-dictionary.netlify.app/",
-        githubUrl: "https://github.com/jgchoti/dictionary-react",
-        linkTitle: "English Dictionary Project create with React",
-        shortDescription: " Built with HTML, CSS with React"
+        name: "Basic Calculator",
+        description: "When I started my journey with HackYourFuture Belgium, I was tasked to created a Basic Calculator. Developed to enhance my web development skills, this calculator is crafted using HTML, CSS, and basic JavaScript.",
+        details: "My goal of this project was to create simple, clean and minimalistic design, making calculations straightforward and efficient. Creating this app meant diving into various functionalities with HTML, CSS, and JavaScript. It was a fun and rewarding challenge that highlights the basics of web development.",
+        imageUrl: process.env.PUBLIC_URL + "/assets/images/calculator.png",
+        alternate_image: process.env.PUBLIC_URL + "/assets/images/calculator_dark.png",
+        linkUrl: "https://choti-calculator.netlify.app/",
+        githubUrl: "https://github.com/jgchoti/basic-calculator",
+        linkTitle: "Basic Calculator",
+        shortDescription: "Built with HTML, CSS and basic JavaScript"
     },
     {
         name: "Bike Shop Website",
@@ -49,6 +49,50 @@ export const projectData = [
         linkUrl: "https://fietsateljee-berchem.netlify.app/",
         githubUrl: "https://github.com/jgchoti/fietsateljee-berchem",
         linkTitle: "Bike Shop Website",
-        shortDescription: " A responsive website, built with HTML, CSS, and JavaScript."
+        shortDescription: "A responsive website, built with HTML, CSS, and JavaScript."
+    },
+    {
+        name: "To-Do List",
+        description: "This to-do list application was developed as part of my studies with HackYourFuture Belgium, aimed at deepening my understanding of JavaScript.",
+        details: "Throughout this project, I aimed to create a user-friendly application where individuals could effortlessly manage their daily tasks. I also implemented local storage capabilities, allowing users to save their to-do lists directly on their devices. This project has not only strengthened my technical skills in JavaScript, particularly in event handling and DOM manipulation, but also taught me the importance of user-centric design.",
+        imageUrl: process.env.PUBLIC_URL + "/assets/images/todo.png",
+        linkUrl: "https://jgchoti.github.io/to-do-list/",
+        githubUrl: "https://github.com/jgchoti/to-do-list",
+        linkTitle: "To-Do List Application",
+        shortDescription: "A responsive To-Do List, built with HTML, CSS, and JavaScript."
+    }, {
+        name: "Studio Ghibli Searcher",
+        description: "The Studio Ghibli Searcher is an application born out of my deep admiration for Studio Ghibli films and characters. It is designed to let users discover the world of Studio Ghibli by using the Studio Ghibli API.",
+        details: "This application was a passion project developed during my studies at HackYourFuture Belgium. One of the highlights of this project was implementing client-side filtering using state management. This feature enables users to refine search results directly.",
+        imageUrl: process.env.PUBLIC_URL + "/assets/images/studio_ghibli.png",
+        alternate_image: process.env.PUBLIC_URL + "/assets/images/studio_ghibli_light.png",
+        linkUrl: "https://jgchoti.github.io/studio-ghibli-searcher/",
+        githubUrl: "https://github.com/jgchoti/studio-ghibli-searcher/",
+        linkTitle: "Studio Ghibli Searcher Application",
+        shortDescription: "A responsive application using the Studio Ghibli API"
+    }, {
+        name: "Potter Searcher",
+        description: "The PotterAPI Searcher is an application developed as part of an assignment during my studies at HackYourFuture Belgium to deepen my understanding of JavaScript and API interactions.",
+        details: "It utilizes the PotterAPI and allow users to explore and discover information about characters, spells, potions, books, and movies. This project served as an opportunity for me to learn about making API calls and managing queries to retrieve specific data from the PotterAPI. One of the key challenges I tackled was implementing server-side filtering to optimize performance and ensure accurate data retrieval.",
+        imageUrl: process.env.PUBLIC_URL + "/assets/images/potter.png",
+        alternate_image: process.env.PUBLIC_URL + "/assets/images/potter_dark.png",
+        linkUrl: "https://jgchoti.github.io/harry-potter-searcher/",
+        githubUrl: "https://github.com/jgchoti/harry-potter-searcher",
+        linkTitle: "Studio Ghibli Searcher Application",
+        shortDescription: "A responsive application using the Studio Ghibli API"
     }
 ];
+
+
+
+// {
+//     name: "English Dictionary",
+//     description: "Dive into the wordy world with this awesome English Dictionary App. So, I wanted to spice up my web development skills, and what better way than crafting this dictionary using React, right? One of the standout features of the app is the random word function. It's like a surprise button that makes learning fun and unexpected!",
+//     details: "Beyond just looking up meanings, users can also get random words thrown their way, making their learning journey exciting and engaging. And hey, the dictionary API integration was a fun challenge to crack.",
+//     imageUrl: process.env.PUBLIC_URL + "/assets/images/dictionary.png",
+//     alternate_image: process.env.PUBLIC_URL + "/assets/images/dictionary_dark.png",
+//     linkUrl: "https://retro-english-dictionary.netlify.app/",
+//     githubUrl: "https://github.com/jgchoti/dictionary-react",
+//     linkTitle: "English Dictionary Project create with React",
+//     shortDescription: " Built with HTML, CSS with React"
+// },
