343fc3a75b9a5e94792114ba232dd153e48530dd
jgchoti <jgchotirat@gmail.com>
2024-06-28 15:22:14 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import '../src/style/index.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 import '@fortawesome/fontawesome-free/css/all.min.css';
+import '../src/style/index.css';
 import App from './component/App';
 import Contact from './component/Contact';
 import reportWebVitals from './reportWebVitals';
