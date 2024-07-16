7ae36ffba74c2d2ca2e816b664bca77f4875c97a
jgchoti <jgchotirat@gmail.com>
2024-07-16 13:41:46 +0200

    Update Hero.js

--- src/component/Hero.js
+++ src/component/Hero.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { ReactTyped } from "react-typed";
 
 function Hero() {
     return (
         <div className="hero">
-            <p>Hi! I am</p>
+            <p><ReactTyped strings={["Hello! I am", "สวัสดีค่ะ ฉันชื่อ", "Hallo! Ik ben", "Hi! My name is", "Dag! Mijn naam is"]} typeSpeed={100} loop /></p>
             <h1>Chotirat Jonggrit</h1>
             <h2>Discover My Project Portfolio!</h2>
             <Link to="/contact" className="btn-branding" title="Contact Choti">Contact Me</Link>
