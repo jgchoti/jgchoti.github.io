b803c4612d903a2f7c04857c097f020dad7e9b7f
jgchoti <jgchotirat@gmail.com>
2024-08-30 15:44:10 +0200

    Update Contact.js

--- src/component/Contact.js
+++ src/component/Contact.js
@@ -2,12 +2,14 @@ import React from 'react';
 import Navbar from './Navbar';
 import Footer from './Footer';
 import ContactContent from './ContactContent';
+import ContactForm from './ContactForm';
 
 function Contact() {
     return (
         <div>
             <Navbar />
             <ContactContent />
+            <ContactForm />
             <Footer />
         </div>
     );
