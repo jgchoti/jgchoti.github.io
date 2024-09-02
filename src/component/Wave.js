739e5ce607d6953c0d4e22cb656d6122334b262f
jgchoti <jgchotirat@gmail.com>
2024-09-02 15:16:55 +0200

    Create Wave.js

---
+++ src/component/Wave.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Wave() {
+    return (
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
+            <path fill="#f5f5db" fillOpacity="1" d="M0,128L80,149.3C160,171,320,213,480,202.7C640,192,800,128,960,117.3C1120,107,1280,149,1360,170.7L1440,192L1440,0L1360,0C1280,0,1120,0,960,0C800,0,640,0,480,0C320,0,160,0,80,0L0,0Z"></path>
+        </svg>
+    )
+}
+
+export default Wave;
+
