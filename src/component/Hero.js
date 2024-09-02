dc2932513a49c0a7d40258c3ed668d5a7e34017f
jgchoti <jgchotirat@gmail.com>
2024-09-02 15:16:40 +0200

    Update Hero.js

--- src/component/Hero.js
+++ src/component/Hero.js
@@ -1,14 +1,26 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { ReactTyped } from "react-typed";
+import Wave from './Wave';
 
 function Hero() {
     return (
-        <div className="hero">
-            <p><ReactTyped strings={["Hello! I am", "สวัสดีค่ะ ฉันชื่อ", "Hallo! Ik ben", "Hi! My name is", "Dag! Mijn naam is"]} typeSpeed={100} loop /></p>
-            <h1>Chotirat Jonggrit</h1>
-            <h2>Discover My Project Portfolio!</h2>
-            <Link to="/contact" className="btn-branding" title="Contact Choti">Contact Me</Link>
+        <div className="hero position-relative text-center">
+            <div className="content-wrapper container">
+                <p>
+                    <ReactTyped
+                        strings={["Hello! I am", "สวัสดีค่ะ ฉันชื่อ", "Hallo! Ik ben", "Hi! My name is", "Dag! Mijn naam is"]}
+                        typeSpeed={100}
+                        loop
+                    />
+                </p>
+                <h1>Chotirat Jonggrit</h1>
+                <h2>Discover My Project Portfolio!</h2>
+                <Link to="/contact" className="btn-branding mb-4" title="Contact Choti">Contact Me</Link>
+            </div>
+            <div className="wave">
+                <Wave />
+            </div>
         </div>
     );
 }
