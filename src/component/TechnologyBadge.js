a455be4e7e71d7e47f0b09cfd781c6ab2125e410
jgchoti <jgchotirat@gmail.com>
2024-09-03 16:02:26 +0200

    Update TechnologyBadge.js

--- src/component/TechnologyBadge.js
+++ src/component/TechnologyBadge.js
@@ -1,8 +1,8 @@
 import React from 'react';
 
-const TechnologyBadge = ({ name, logoUrl }) => {
+const TechnologyBadge = ({ name, logoUrl, index }) => {
     return (
-        <div className="tech-badge">
+        <div className="tech-badge" key={index}>
             <img src={logoUrl} alt={name} className="tech-logo" />
             <div className="tech-name">{name}</div>
         </div>
