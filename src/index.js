98c9736e071f72ef5d2f3e778018910fe2aa1bce
jgchoti <jgchotirat@gmail.com>
2024-10-10 16:02:07 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -5,11 +5,12 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 import '@fortawesome/fontawesome-free/css/all.min.css';
 import '../src/style/index.css';
-import App from './component/App';
-import Contact from './component/Contact';
+import App from './pages/App';
+import Contact from './pages/Contact';
 import reportWebVitals from './reportWebVitals';
-import About from './component/About';
-import Project from './component/Project';
+import About from './pages/About';
+import Project from './pages/Project';
+import Blog from './pages/Blog';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -19,6 +20,7 @@ root.render(
         <Route path="/" element={<App />} />
         <Route path="/about" element={<About />} />
         <Route path="/project" element={<Project />} />
+        <Route path="/blog" element={<Blog />} />
         <Route path="/contact" element={<Contact />} />
       </Routes>
     </BrowserRouter>
