1866c2e6963168790190c27fb3f9dcb186f5a341
jgchoti <jgchotirat@gmail.com>
2024-10-10 16:02:14 +0200

    Create Blog.js

---
+++ src/pages/Blog.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Navbar from '../component/Navbar';
+import BlogBody from '../component/BlogBody';
+
+function Blog() {
+    return (
+        <div>
+            <Navbar />
+            <BlogBody />
+        </div>
+    );
+}
+
+export default Blog;
