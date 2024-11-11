6953d2c04ae80377b66e4b9ad6845894277c1a91
jgchoti <jgchotirat@gmail.com>
2024-11-11 22:27:46 +0100

    Update SubHeader.js

--- src/component/SubHeader.js
+++ src/component/SubHeader.js
@@ -8,7 +8,7 @@ function SubHeader() {
         <div className='sub-header container-fluid w-100 m-0'>
             <div className="text-center sub-header-text">
                 <h4>I build and share innovative solutions through tech projects and case studies.</h4>
-                <h4>Discover my <a href="/project" className="button-link"><strong >Project</strong></a>, learn <a href="/about" className="button-link"><strong >About Me</strong></a>, or dive into my <a href="/blog" className="button-link"><strong >Blog</strong></a></h4>
+                <h4>Discover my <a href="/project"><strong >Project</strong></a>, learn <a href="/about"><strong >About Me</strong></a>, or dive into my <a href="/blog"><strong >Blog</strong></a></h4>
             </div>
             <div className="col-lg-1">
             </div>
