6b277ed6ce0941780636f9a7b1bbbbead8abb190
jgchoti <jgchotirat@gmail.com>
2025-05-20 16:47:42 +0200

    Update App.js

--- src/pages/App.js
+++ src/pages/App.js
@@ -1,28 +1,27 @@
-import React from 'react';
-import Navbar from '../component/Navbar.js';
+import React, { useEffect } from 'react';
 import Hero from '../component/Hero.js';
-import SubHeader from '../component/SubHeader.js';
+// import SubHeader from '../component/SubHeader.js';
 import Footer from '../component/Footer.js';
 import ContactBox from '../component/ContactBox.js';
-import ScrollReveal from '../logic/ScrollReveal.js';
 import '../style/index.css';
 import '../style/App.css'
 import Content from '../component/Content.js';
-import TechnologySum from '../component/TechnologySum.js';
 import { projectData } from '../data/projectData.js';
-
+import { useTheme } from '../component/Theme.js';
 
 function App() {
+  const { theme } = useTheme();
+
+  useEffect(() => {
+    document.documentElement.setAttribute('data-theme', theme);
+  }, [theme]);
+
   return (
     <div>
-      <Navbar />
       <Hero />
-      <SubHeader />
-      <TechnologySum projectData={projectData} />
       <Content projectData={projectData} />
       <ContactBox />
       <Footer />
-      <ScrollReveal />
     </div>
   );
 }
