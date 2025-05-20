6777a14ee138bd2802f70afd7919a6ab8e516b02
jgchoti <jgchotirat@gmail.com>
2025-05-20 16:48:01 +0200

    Update ProjectHeader.js

--- src/component/ProjectHeader.js
+++ src/component/ProjectHeader.js
@@ -1,11 +1,17 @@
 import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faAnglesDown } from '@fortawesome/free-solid-svg-icons'
+import { useTheme } from './Theme';
 
 function ProjectHeader() {
+    const theme = useTheme()
+    const isWeb = theme.theme === 'web';
+
     return (
-        <div className="hero mt-4 mb-4">
-            <h1>Explore My Project</h1>
+        <div className="hero mb-4">
+            <h1>
+                {isWeb ? 'Pixels & Interaction — Web Projects' : 'Patterns & Insights — Data Lab'}
+            </h1>
             <div className='scroll-text-wrapper'>
                 <FontAwesomeIcon icon={faAnglesDown} />
                 <h4>scroll down to see more</h4>
