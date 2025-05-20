6e155aba025fd82d88a57f7997fd33d3a677a20b
jgchoti <jgchotirat@gmail.com>
2025-05-20 16:48:10 +0200

    Create Theme.js

---
+++ src/component/Theme.js
@@ -0,0 +1,26 @@
+import { createContext, useContext, useEffect, useState } from 'react';
+import { useLocation } from 'react-router-dom';
+
+const ThemeContext = createContext();
+
+export const ThemeProvider = ({ children }) => {
+    const [theme, setTheme] = useState('web'); // default
+    const location = useLocation();
+
+    useEffect(() => {
+        if (location.pathname.startsWith('/data')) {
+            setTheme('data');
+        } else if (location.pathname.startsWith('/project')) {
+            setTheme('web');
+        }
+    }, [location]);
+
+    return (
+        <ThemeContext.Provider value={{ theme, setTheme }}>
+            {children}
+        </ThemeContext.Provider>
+    );
+};
+
+
+export const useTheme = () => useContext(ThemeContext);
