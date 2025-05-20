8d058e430ec8948fc4ee8b4ffefdff1b85a9a235
jgchoti <jgchotirat@gmail.com>
2025-05-20 16:48:14 +0200

    Update SubHeader.js

--- src/component/SubHeader.js
+++ src/component/SubHeader.js
@@ -1,7 +1,7 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
-import Wave from 'react-wavify'
-import profileIllustration from '../assets/images/profile_illustration.png';
+// import { Link } from 'react-router-dom';
+// import Wave from 'react-wavify'
+// import profileIllustration from '../assets/images/profile_illustration.png';
 
 function SubHeader() {
     return (
