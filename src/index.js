4cee0f477a651fe58f3021f9e86135a97f2f5732
jgchoti <jgchotirat@gmail.com>
2025-05-20 16:47:49 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -4,10 +4,12 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 import '@fortawesome/fontawesome-free/css/all.min.css';
-import '../src/style/index.css';
+import './style/index.css';
+import { ThemeProvider } from './component/Theme'
+
+import Layout from './component/Layout'
 import App from './pages/App';
 import Contact from './pages/Contact';
-import reportWebVitals from './reportWebVitals';
 import About from './pages/About';
 import Project from './pages/Project';
 import Blog from './pages/Blog';
@@ -16,15 +18,18 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<App />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/project" element={<Project />} />
-        <Route path="/blog" element={<Blog />} />
-        <Route path="/contact" element={<Contact />} />
-      </Routes>
+      <ThemeProvider>
+        <Routes>
+          <Route path="/" element={<Layout />}>
+            <Route index element={<App />} />
+            <Route path="about" element={<About />} />
+            <Route path="project" element={<Project />} />
+            <Route path="data" element={<Project />} />
+            <Route path="blog" element={<Blog />} />
+            <Route path="contact" element={<Contact />} />
+          </Route>
+        </Routes>
+      </ThemeProvider>
     </BrowserRouter>
   </React.StrictMode>
 );
-
-reportWebVitals();
