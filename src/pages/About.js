67fb665d98fc09d4d2605c37839bd98cdc26a958
jgchoti <jgchotirat@gmail.com>
2025-05-20 16:47:44 +0200

    Update About.js

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,15 +1,11 @@
 import React from 'react';
-import Navbar from '../component/Navbar';
-import ContactBox from '../component/ContactBox'
 import Footer from '../component/Footer';
 import AboutContent from '../component/AboutContent';
 
 function About() {
     return (
         <div>
-            <Navbar />
             <AboutContent />
-            <ContactBox />
             <Footer />
         </div>
     );
