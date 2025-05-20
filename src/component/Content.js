06b4c704f1e6819923e127db03bd206a3ffbb6c7
jgchoti <jgchotirat@gmail.com>
2025-05-20 16:49:08 +0200

    Update Content.js

--- src/component/Content.js
+++ src/component/Content.js
@@ -1,10 +1,13 @@
 import ProjectShowcase from './ProjectShowcase';
+import { useTheme } from './Theme'
 
 function Content(props) {
+    const { theme } = useTheme();
+    const filteredProjects = props.projectData.filter(project => project.type === theme);
     return (
         <div className="container projects-container">
             <div className="row mt-5">
-                {props.projectData.map((project, index) => (
+                {filteredProjects.length > 0 ? (filteredProjects.map((project, index) => (
                     <ProjectShowcase
                         key={index}
                         index={index}
@@ -16,8 +19,10 @@ function Content(props) {
                         githubUrl={project.githubUrl}
                         blogUrl={project.blogUrl}
                         webUrl={project.webUrl}
-                    />
-                ))}
+                    />)
+                )) : (<div className="text-center mt-5 mb-5" style={{ fontStyle: 'italic', opacity: 0.7 }}>
+                    🚧 Ongoing — more projects coming soon!
+                </div>)}
             </div>
         </div>
     );
