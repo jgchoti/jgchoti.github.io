20ca84fba3b87c2a28b242cb30c1a7402486bc5b
jgchoti <jgchotirat@gmail.com>
2025-05-20 16:47:40 +0200

    Update Blog.js

--- src/pages/Blog.js
+++ src/pages/Blog.js
@@ -1,11 +1,9 @@
 import React from 'react';
-import Navbar from '../component/Navbar';
 import BlogBody from '../component/BlogBody';
 
 function Blog() {
     return (
         <div>
-            <Navbar />
             <BlogBody />
         </div>
     );
