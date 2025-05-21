29871ce088cbaf72e32990b1774f5516e408422c
jgchoti <jgchotirat@gmail.com>
2025-05-21 11:25:08 +0200

    Update TabSwitcher.js

--- src/component/TabSwitcher.js
+++ src/component/TabSwitcher.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { motion } from "framer-motion";
-import { useTheme } from "./Theme";
+import { useTheme } from "../logic/Theme";
 
 const tabs = [
     { icon: "💻", label: "Web" },
