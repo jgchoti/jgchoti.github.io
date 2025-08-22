94439bb27479ecd07bb98764571f639aa80c7d90
jgchoti <jgchotirat@gmail.com>
2025-08-22 20:08:26 +0200

    edit tagline

--- src/component/Hero.js
+++ src/component/Hero.js
@@ -10,34 +10,8 @@ function Hero() {
         <div className="hero m-0">
             <div className="content-wrapper container">
                 <p>Hi! I am Choti.</p>
-                <h1>I work with pixels and patterns.</h1>
+                <h1>I build with pixels and pipelines.</h1>
                 <TabSwitcher />
-                {/* <button
-                    aria-label="Toggle between Web and Data projects"
-                    role="switch"
-                    aria-checked={isWeb}
-                    onClick={handleToggle}
-                    onKeyDown={(e) => (e.key === "Enter" || e.key === " ") && handleToggle()}
-                    className="toggle-button"
-                    style={{
-                        "--toggle-bg": isWeb ? "#687856" : "#552205",
-                    }}
-                >
-                    <motion.div
-                        layout
-                        transition={spring}
-                        className="toggle-knob"
-                        style={{
-                            left: isWeb ? 4 : "calc(100% - 40px)",
-                            color: isWeb ? "#687856" : "#f9d6d6",
-                        }}
-                    >
-                        {isWeb ? "💻" : "📊"}
-                    </motion.div>
-
-                    <span className={`label data ${!isWeb ? "active" : "inactive"}`}>Data</span>
-                    <span className={`label web ${isWeb ? "active" : "inactive"}`}>Web</span>
-                </button> */}
             </div>
         </div>
     );
